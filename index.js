c3661a19edcf92635090e48625c41f7d88fd5b25
BarbaraDascoli <b.dascoli@outlook.it>
2021-02-20 16:23:55 +0100

    this is me added to line 6

--- index.js
+++ index.js
@@ -3,4 +3,4 @@ import './style.css';
 
 // Write Javascript code!
 const appDiv = document.getElementById('app');
-appDiv.innerHTML = `<h1>hello world!</h1>`;
+appDiv.innerHTML = `<h1>hello world this is me!</h1>`;
